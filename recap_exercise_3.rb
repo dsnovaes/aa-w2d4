b72ca700005f710f18c708be8d8e8f69b864b929
Diego Novaes <dsnovaes1@gmail.com>
2022-10-13 20:48:38 -0700

    recap 3 until procs

--- recap_exercise_3.rb
+++ recap_exercise_3.rb
@@ -126,13 +126,50 @@ end
 
 class String
 
-    def self.select(&prc)
-        
+    def select(&prc)
+        if !prc
+            ""
+        else
+            new_str = ""
+            self.each_char { |char| new_str += char if prc.call(char) }
+            new_str
+        end
+    end
+
+    def map!(&prc)
+        self.each_char.with_index { |char,i| self[i] = prc.call(char,i) }
     end
 
 end
 
+# # Examples
+# p "app academy".select { |ch| !"aeiou".include?(ch) }   # => "pp cdmy"
+# p "HELLOworld".select { |ch| ch == ch.upcase }          # => "HELLO"
+# p "HELLOworld".select          # => ""
+
 # Examples
-p "app academy".select { |ch| !"aeiou".include?(ch) }   # => "pp cdmy"
-p "HELLOworld".select { |ch| ch == ch.upcase }          # => "HELLO"
-p "HELLOworld".select          # => ""
+word_1 = "Lovelace"
+word_1.map! do |ch| 
+    if ch == 'e'
+        '3'
+    elsif ch == 'a'
+        '4'
+    else
+        ch
+    end
+end
+p word_1        # => "Lov3l4c3"
+
+word_2 = "Dijkstra"
+word_2.map! do |ch, i|
+    if i.even?
+        ch.upcase
+    else
+        ch.downcase
+    end
+end
+p word_2        # => "DiJkStRa"
+
+
+# Recursion Hell
+
